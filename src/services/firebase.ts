003e1a4199eb6d4f1494b2b42cb5717c8e46cedb
Luis-Felipe-N <luisj2felipe09@gmail.com>
2021-10-17 17:04:39 -0300

    add dotenv

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -4,12 +4,12 @@ import { getAuth } from 'firebase/auth'
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC5UbhgngCI8WjXOFAzMgC_VladuDyQHeM",
-  authDomain: "closser-fb5a5.firebaseapp.com",
-  projectId: "closser-fb5a5",
-  storageBucket: "closser-fb5a5.appspot.com",
-  messagingSenderId: "938206560673",
-  appId: "1:938206560673:web:1c7dfc250c58a511378be0"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTHDOMIAN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
 
 // Initialize Firebase
